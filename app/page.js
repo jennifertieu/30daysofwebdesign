6b54f8c26e4a44e00a7a5d3b5e60d75a3989c20e
Jennifer Tieu <jennifer.tieuu@gmail.com>
2023-09-12 23:14:42 -0500

    make responsive, set link to title

--- app/page.js
+++ app/page.js
@@ -4,19 +4,24 @@ import Footer from "./components/Footer";
 
 export default function Home() {
   return (
-    <main className="py-8 max-w-screen-xl m-auto flex flex-col h-screen">
+    <main className="py-8 max-w-screen-xl m-auto flex flex-col h-screen px-4">
       <h1 className="text-center text-2xl font-bold my-8">
         #30DaysOfWebDesign Challenges
       </h1>
-      <section className="grid grid-cols-3 auto-rows-min grow">
-        <Link className="p-4 border rounded" href="/day/1">
-          <h2 className="font-bold">Day 1</h2>
+      <section className="grid grid-cols-1 md:grid-cols-3 auto-rows-min grow ">
+        <article className="p-4 border rounded">
+          <Link
+            className="font-bold hover:underline text-blue-600"
+            href="/day/1"
+          >
+            Day 1
+          </Link>
           <p>
             Design a hero section for a website that helps you find your new
             home, away from the stress of the city or you can always get a
             random image from Unsplash and make your design based on the image.
           </p>
-        </Link>
+        </article>
       </section>
       <Footer />
     </main>
